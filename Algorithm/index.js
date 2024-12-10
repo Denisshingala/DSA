58fab557d62c30d1268128fdd874c8f4ee77b42e
Denis Shingala <denisshingala@gmail.com>
2024-12-10 23:04:00 +0530

    10, Dec 2024 - Next permutation algorithm.

--- Algorithm/index.js
+++ Algorithm/index.js
@@ -20,3 +20,42 @@ const findMaxSubArraySum = (arr) => {
 
 let arr = [-2, -3, 4, -1, -2, 1, 5, -3];
 console.log(findMaxSubArraySum(arr));
+
+/**
+ * Next Permutation
+ *
+ * Link: https://leetcode.com/problems/next-permutation/description/
+ */
+
+const getNextPermutation = (arr) => {
+    let ind = -1, length = arr.length;
+
+    for(let i = length - 2; i>=0; i--) {
+        if(arr[i] < arr[i+1]) {
+            ind = i;
+            break;
+        }
+    }
+
+    if(ind == -1) {
+        arr.reverse();
+        return arr;
+    }
+
+    for(let i = length - 1; i >= ind; i--){
+        if(arr[i] > arr[ind]) {
+            let temp = arr[i];
+            arr[i] = arr[ind];
+            arr[ind] = temp;
+            break;
+        }
+    }
+
+    let leftPart = arr.slice(0, ind+1);
+    let rightPart = arr.slice(ind+1).reverse();
+
+    return [...leftPart, ...rightPart];
+}
+
+arr = [5,4,3,2,1];
+console.log(getNextPermutation(arr));
